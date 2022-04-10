e9ad4aa27ed1a7499944a4e22ba800194cadce09
Vishnu Prasad Preetham Kumar <81408708+Wish-nu@users.noreply.github.com>
2022-04-10 16:03:15 +0530

    Create PayMoney.java

---
+++ PayMoney.java
@@ -0,0 +1,51 @@
+package com.support.paymoney;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of transaction array: ");
+        int numOfTransactions = sc.nextInt();
+        int[] txnValues = new int[numOfTransactions];
+        int[] cltValues = new int[numOfTransactions];
+        System.out.println("Enter the values of the array: ");
+
+        for (int i=0; i<numOfTransactions; i++){
+            txnValues[i] = sc.nextInt();
+            cltValues[i] = ((i==0)?0: cltValues[i-1])+txnValues[i];
+        }
+        System.out.println("Cumulative Values: " + Arrays.toString(cltValues));
+        System.out.println("Enter the total no. of targets that needs to be achieved: ");
+        int numOfTargets = sc.nextInt();
+        for (int i=0; i<numOfTargets; i++){
+            System.out.println("Enter the value of Target"+ (i+1)+": ");
+            int target = sc.nextInt();
+            int reqNoOfTxnForTargt = getTxnForTarget(cltValues, 0, numOfTransactions-1, target);
+            System.out.println(reqNoOfTxnForTargt < 0? "Target is not achievable ":"Target achieved in " + reqNoOfTxnForTargt + " transactions");
+
+        }
+        sc.close();
+    }
+
+    private static int getTxnForTarget(int[] cltValues, int i, int i1, int target) {
+        if(cltValues[cltValues.length-1]<target){
+            return -1;
+        }
+        if(cltValues[0]>target){
+            return 1;
+        }
+        if(i<i1){
+            return -1;
+        }
+        int mid = (i+i1)/2;
+        if(target<= cltValues[mid]){
+            return target>cltValues[mid-1]?mid+1:getTxnForTarget(cltValues, mid+1,i1, target);
+
+        }
+
+    return -1;
+    }
+}
